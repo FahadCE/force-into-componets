7476f61951cdb064d7d61e27e41ecdb07edf9d76
FahadCE <69978169+FahadCE@users.noreply.github.com>
2020-08-20 20:56:01 +0500

    Add files via upload

---
+++ trignometery.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 20 19:37:25 2020
+
+@author: FAHAD ANEES
+"""
+
+import math as m
+def baseComponent(F,theta):
+    fx=F*m.cos(theta*(m.pi)/180)
+    fy=F*m.sin(theta*(m.pi)/180)
+    print('fx='+str(round(fx,2)))
+    print('fy='+str(round(fy,2)))
